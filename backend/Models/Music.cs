b83b20da14487e8aa4ab6fdbd28deccfa0782287
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-03-20 13:50:51 -0300

    feat: creating Music model

---
+++ backend/Models/Music.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class Music
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Artist { get; set; }
+        public required string Url { get; set; }
+        public DateTime UploadedAt { get; set; }
+    }
+}
